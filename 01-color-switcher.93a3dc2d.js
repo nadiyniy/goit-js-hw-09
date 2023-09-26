4ca6dfaa8ebd6d916f0483f1aa6f199d801b766b
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-26 09:57:13 +0000

    Deploying to gh-pages from @ nadiyniy/goit-js-hw-09@98d5bc9830ed409b827447279b2c4df35926a151 🚀

---
+++ 01-color-switcher.93a3dc2d.js
@@ -0,0 +1,2 @@
+document.querySelector(button[data-start]),document.querySelector(button[data-stop]);
+//# sourceMappingURL=01-color-switcher.93a3dc2d.js.map
---
+++ 01-color-switcher.cbd57b7f.js
@@ -0,0 +1,2 @@
+document.querySelector(button[data-start]),document.querySelector(button[data-stop]);
+//# sourceMappingURL=01-color-switcher.cbd57b7f.js.map
