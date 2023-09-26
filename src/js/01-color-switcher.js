98d5bc9830ed409b827447279b2c4df35926a151
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-26 12:56:52 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+const btnStart = document.querySelector(button[data-start]);
+const btnStop = document.querySelector(button[data-stop]);
 
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+
+  
