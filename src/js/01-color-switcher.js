0fd7b388f76a92ac88a6cc42b59cbbb733b47c6f
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-26 13:05:17 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,11 +1,16 @@
-const btnStart = document.querySelector(button[data-start]);
-const btnStop = document.querySelector(button[data-stop]);
-
-
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const onClickStart = () => {
+    
+};
+const onClickStop = () => {};
 
-  
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
