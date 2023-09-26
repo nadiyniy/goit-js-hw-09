ac5a5262a87be09fd91f604228d4cd3448fb54c3
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-26 13:34:27 +0300

    Completed Task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,8 @@
 const btnStart = document.querySelector('button[data-start]');
 const btnStop = document.querySelector('button[data-stop]');
+const body = document.body;
+let isActive = false;
+let interval = null;
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
@@ -8,9 +11,24 @@ function getRandomHexColor() {
 }
 
 const onClickStart = () => {
-    
+  if (!isActive) {
+    isActive = true;
+    interval = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  btnStart.disabled = true;
+  btnStop.disabled = false;//не обов'язково
 };
-const onClickStop = () => {};
+
+const onClickStop = () => {
+  if (isActive) {
+    isActive = false;
+    clearInterval(interval);
+    btnStart.disabled = false;
+  }
+  btnStop.disabled = true; //не обов'язково
+};
 
 btnStart.addEventListener('click', onClickStart);
 btnStop.addEventListener('click', onClickStop);
